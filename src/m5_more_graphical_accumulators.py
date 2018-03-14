acb7bf2b9387daaac18facf6763c526e699a3d94
zhangz14 <zhangz14@rose-hulman.edu>
2018-03-14 07:51:36 -0400

    1

--- src/m5_more_graphical_accumulators.py
+++ src/m5_more_graphical_accumulators.py
@@ -375,8 +375,6 @@ def draw_lines_from_rectangles(rectangle1, rectangle2, n, window):
     window.render()
 
 
-
-
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
